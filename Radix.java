c0a9752b72c4d4d7c4f4ac751a62a4bcbd575599
clee010922 <clee01@stuy.edu>
2019-04-03 16:41:14 -0400

    fixed getNthDigit and creates a bucket at the start of the code

--- Radix.java
+++ Radix.java
@@ -1,30 +1,30 @@
 public class Radix {
 
+  @SuppressWarnings("unchecked")
   public static void radixsort(int[] data) {
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    for (int i = 0; i < buckets.length; i++) {
+      buckets[i] = new MyLinkedList<Integer>();
+    }
     int max = 0;
-    MyLinkedList values = new MyLinkedList();
     for (int i = 0; i < data.length; i++) {
-      values.add(data[i]);
       if (data[i] > max)
         max = data[i];
     }
-    int k = 1;
-    while (k > 10) {
+    int k = 0;
+    while (max > 0) {
       max /= 10;
       k++;
     }
-    int n = k;
     while (k > 0) {
       for (int i = 0; i < k; i++) {
-        MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
-        buckets[getNthDigit(data[i], n++)].add(data[i]);
       }
     }
   }
 
   private static int getNthDigit(int val, int n) {
-    int temp = (int)Math.pow(10, n);
-    return val % temp;
+    int result = (val / (int)Math.pow(10, n)) % 10;
+    return result;
   }
 
 }
